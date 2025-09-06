94d1fc6c24aef62ad6e9753a53b43fc13117bb3b
Vladyslav Kostiuk <K4nezoor@gmail.com>
2025-09-06 11:52:40 +0200

    changed bit method logic

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -22,13 +22,12 @@ class Herbivore extends Animal {
 
 class Carnivore extends Animal {
   bite(animal) {
-    if (animal instanceof Carnivore || animal.hidden) {
-      return;
-    }
-    animal.health -= 50;
+    if (animal instanceof Herbivore && !animal.hidden) {
+      animal.health -= 50;
 
-    if (animal.health <= 0) {
-      Animal.alive = Animal.alive.filter((anim) => anim.health > 0);
+      if (animal.health <= 0) {
+        Animal.alive = Animal.alive.filter((anim) => anim.health > 0);
+      }
     }
   }
 }
